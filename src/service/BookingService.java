77242d589f69b6fe69aac622622e4c2f2cb11b59
gadodiyas <sheetalgadodiya31@gmail.com>
2023-03-12 15:34:14 +0530

    First commit

--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -10,20 +10,22 @@ import java.util.TreeMap;
 public class BookingService {
     //to do make interface for booking service
 
-    public void synchronized bookTable(Restaurant restaurant, int userId, int noOfPersons, LocalDateTime time) {
+    public void  bookTable(Restaurant restaurant, int userId, int noOfPersons, LocalDateTime time) {
         //check if slot is available
         TreeMap<LocalDateTime, Integer> availability = restaurant.getAvailableSlots();
-        if(availability.containsKey(time)){
-            Booking booking = new Booking(time, restaurant.getId(), userId, noOfPersons);
-            int no = availability.get(time);
-            no--;
-            if(no == 0){
-                availability.remove(time);
-            } else {
-                availability.put(time, no);
-            }
-            System.out.println("Booked restaurant booked" + booking.toString());
+        synchronized(availability) {
+            if (availability.containsKey(time)) {
+                Booking booking = new Booking(time, restaurant.getId(), userId, noOfPersons);
+                int no = availability.get(time);
+                no--;
+                if (no == 0) {
+                    availability.remove(time);
+                } else {
+                    availability.put(time, no);
+                }
+                System.out.println("Booked restaurant booked" + booking.toString());
 
+            }
         }
     }
 }
